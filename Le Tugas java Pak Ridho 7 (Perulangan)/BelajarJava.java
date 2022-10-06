ca7e90b4bf827eca57cabed2f105825ade96eef0
Test2 <105333429+AlraziTest1@users.noreply.github.com>
2022-10-06 10:46:15 +0700

    Le tugas java pak ridho 7

---
+++ Le Tugas java Pak Ridho 7 (Perulangan)/BelajarJava.java
@@ -0,0 +1,11 @@
+public class BelajarJava {
+	public static void main(String args[]){
+		int a = 9; //	nilai int a adalah 12
+		int b = 10; //	nilai int b adalah 10
+		
+		if (a > b) {	//	memanggil funsgsi if "jika nilai variabel a lebih besar dari variabel b,maka jalankan {...}"
+			System.out.println("Nilai variabel a lebih besar dari bariabel b");	/*	yang akan dijalankan bila kondisi
+																					fungsi "if" terpenuhi*/
+		}
+	}
+}
---
+++ Le Tugas java Pak Ridho 7 (Perulangan)/BelajarJava2.java
@@ -0,0 +1,21 @@
+public class BelajarJava2 {
+	public static void main(String args[]){
+		int a = 5;	//	nilai int a adalah 5
+		int b = 10;	//	nilai int a adalah 10
+		
+		if (a > b) {	//	memanggil funsgsi if "jika nilai variabel a lebih besar dari variabel b,maka jalankan {...}"
+			System.out.println("Nilai variable a lebih besar dari variabel b");	/*	yang akan dijalankan bila kondisi
+																					fungsi "if" terpenuhi*/
+		}
+		if (a < b) {	//	memanggil funsgsi if "jika nilai variabel a kecil besar dari variabel b,maka jalankan {...}"
+			System.out.println("Nilai variable a lebih kecil dari variabel b");	/*	yang akan dijalankan bila kondisi
+																					fungsi "if" terpenuhi*/
+		}
+		if (a == b) {	//	memanggil funsgsi if "jika nilai variabel a bernilai sama dengan variabel b,maka jalankan {...}"
+			System.out.println("Nilai variable a sama dengan variabel b");		/*	yang akan dijalankan bila kondisi
+																					fungsi "if" terpenuhi*/
+		}
+		
+	}
+}
+		
