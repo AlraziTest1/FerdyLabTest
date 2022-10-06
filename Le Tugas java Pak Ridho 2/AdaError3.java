d6fd97086fd855829380a468ade6e6b09d63440b
Test2 <105333429+AlraziTest1@users.noreply.github.com>
2022-10-06 10:42:22 +0700

    Le tugas java 2
    tugas java 2 pak ridho

---
+++ Le Tugas java Pak Ridho 2/AdaError3.java
@@ -0,0 +1,7 @@
+public class AdaError3{
+	public static void main(String[] args){
+		System.out.println("banyak error");
+		System.out.println("tapi ferdy punya lebih banyak le akal");
+		System.out.println("Le benar");
+	}
+}
---
+++ Le Tugas java Pak Ridho 2/HeloWords.java
@@ -0,0 +1,5 @@
+public class HeloWords{
+	public static void main(String[] args){
+		System.out.println("Hello World");
+	}
+}	
