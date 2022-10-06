9277ab68b4b5c1bf5b0201121eecb965ac91c22d
Test2 <105333429+AlraziTest1@users.noreply.github.com>
2022-10-06 10:45:43 +0700

    Le tugas java pak ridho 6

---
+++ Le Tugas java Pak Ridho 6 (Perbandingan)/Belajarperbandingan.java
@@ -0,0 +1,32 @@
+public class Belajarperbandingan{
+	public static void main(String args[]){
+		int a = 25;
+		int b = 25;
+		boolean jancokPristel;
+		
+		jancokPristel = a == b;
+		System.out.println("Apakah a == b ? " + jancokPristel);
+		
+		
+		jancokPristel = a != b;
+		System.out.println("Apakah a != b ? " + jancokPristel);
+		
+		
+		jancokPristel = a > b;
+		System.out.println("Apakah a > b ? " + jancokPristel);
+		
+		
+		jancokPristel = a < b;
+		System.out.println("Apakah a < b ? " + jancokPristel);
+		
+		
+		jancokPristel  = a >= b;
+		System.out.println("Apakah a >= b ? " + jancokPristel);
+		
+		
+		jancokPristel = a <= b;
+		System.out.println("Apakah a <= b ? " + jancokPristel);
+		
+	}
+	
+}
---
+++ Le Tugas java Pak Ridho 6 (Perbandingan)/Belajarperbandingan2.java
@@ -0,0 +1,32 @@
+public class Belajarperbandingan2{
+	public static void main(String args[]){
+		int c = 35;
+		int d = 40;
+		boolean jancokPristel;
+		
+		jancokPristel = c == d;
+		System.out.println("Apakah c == d ? " + jancokPristel);
+		
+		
+		jancokPristel = c != d;
+		System.out.println("Apakah c != d ? " + jancokPristel);
+		
+		d
+		jancokPristel = c > d;
+		System.out.println("Apakah c > d ? " + jancokPristel);
+		
+		
+		jancokPristel = c < d;
+		System.out.println("Apakah c < d ? " + jancokPristel);
+		
+		
+		jancokPristel  = c >= d;
+		System.out.println("Apakah c >= d ? " + jancokPristel);
+		
+		
+		jancokPristel = c <= d;
+		System.out.println("Apakah c <= d ? " + jancokPristel);
+		
+	}
+	
+}
